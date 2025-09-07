e010af4a157f36554de88411799768cec114a4f4
fkirimi <f.kirimi@outlook.com>
2025-09-07 17:10:33 +0300

    Create forms.py

---
+++ forms.py
@@ -0,0 +1,8 @@
+import streamlit as st
+from datetime import datetime
+from utils.file_handlers import load_json_data, save_json_data
+from utils.categories import get_subcategories_for_category, load_income_categories
+
+def render_transaction_form():
+    # Your form rendering logic here
+    pass
